a423decc5156042d7debbc30fcdb90be2abe3de5
BadRobot-34 <147645367+BadRobot-34@users.noreply.github.com>
2024-10-28 00:34:09 +0100

    Create live.php

---
+++ live.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <title>Canales en Vivo</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="live.js" defer></script> <!-- Usaremos un nuevo script live.js -->
+</head>
+<body>
+    <header>
+        <h1>EN VIVO</h1>
+    </header>
+    <main id="canales-container">
+        <div class="loader">Cargando canales...</div>
+    </main>
+    <br>
+    <br>
+    <br>
+    <br>
+    <footer>
+        <div class="menu-footer">
+            <div class="menu-item">
+                <a href="index.php">
+                    <img src="img/incio.png" alt="Inicio">
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="epg.php">
+                    <img src="img/programacion.png" alt="Programación">
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="live.php">
+                    <img src="img/live.png" alt="Live">
+                </a>
+            </div>
+        </div>
+        <p>🇪🇸 CREADO POR FIRULAIS 🇪🇸</p>
+    </footer>
+</body>
+</html>
